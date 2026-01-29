82a09c1189789aa8396a0137df926f2b57e1928c
dswithai <dswithai@gmail.com>
2026-01-29 10:32:25 +0530

    Update __init__.py

--- build/lib/pyneatR/__init__.py
+++ build/lib/pyneatR/__init__.py
@@ -1,5 +1,6 @@
 from .numbers import nnumber, npercent
 from .strings import nstring
 from .dates import ndate, ntimestamp, nday
+from .f import f
 
-__all__ = ["nnumber", "npercent", "nstring", "ndate", "ntimestamp", "nday"]
+__all__ = ["nnumber", "npercent", "nstring", "ndate", "ntimestamp", "nday", "f"]
